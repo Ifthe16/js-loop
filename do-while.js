b199519393628370d3c41077f2f8cc3b4ad53a2a
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-05 01:51:08 +0600

    added the do-while file of javascript

---
+++ do-while.js
@@ -0,0 +1,24 @@
+// almost use korbo na
+
+// while loop
+let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// do while loop
+let n = 0;
+do {
+    console.log(n);
+    n++;
+}
+while (n < 5)
+
+// example 02    
+let m = 10;
+do {
+    console.log(m);
+    m++;
+}
+while (m < 5)
--- while.js
+++ while.js
@@ -6,7 +6,7 @@ console.log('something new is cooking');
 
 // ------------WHILE---LOOP_____________
 let num = 0; // loop variable
-while (num < 5) {
+while (num < 5) { // loop condition
     console.log('looooooooping', num);
     // num = num + 1;
     // num += 1;
