8cfadb77cbe7de83cde710fc9800f535eab53ae9
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-04 12:18:54 +0600

    added sum and even file of the javascript

---
+++ even.js
@@ -0,0 +1,20 @@
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+
+    if (num % 2 === 0) {
+        console.log('even number', num);
+    }
+    num++;
+}
+
+// example 02
+
+let num2 = 1;
+while (num2 <= 10) {
+
+    if (num2 % 2 === 0) {
+        console.log('even number', num2);
+    }
+    num2++;
+}
